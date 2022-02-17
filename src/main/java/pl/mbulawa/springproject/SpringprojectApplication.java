d1859a5cf6177e9a2e12188720cdaf95e8954e19
Mateusz Bulawa <buleeeeek@gmail.com>
2022-02-17 21:47:25 +0100

    Initial commit

---
+++ src/main/java/pl/mbulawa/springproject/SpringprojectApplication.java
@@ -0,0 +1,13 @@
+package pl.mbulawa.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mbulawa/springproject/SpringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mbulawa.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
